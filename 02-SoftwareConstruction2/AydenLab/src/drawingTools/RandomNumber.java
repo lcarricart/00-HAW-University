3ce0ac2e1a63bd78da78a25d4cc04f746fab064a
Luciano Carricart <wzv135@haw-hamburg.de>
2025-10-07 21:52:46 +0200

    Deleted programs

--- 02-SoftwareConstruction2/AydenLab/src/drawingTools/RandomNumber.java
+++
@@ -1,7 +0,0 @@
-package drawingTools;
-
-public class RandomNumber {     //I dont use this
-    public static int between(int min, int max) {
-        return (int) (Math.random() * ((max - min) + 1) + min);
-    }
-}
